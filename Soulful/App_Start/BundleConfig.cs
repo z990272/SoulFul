feb49aaf618dc450005126ad06aca1c444be8357
2opex <jerry1003tw@gmail.com>
2020-05-18 20:44:40 +0800

    navbar bundle

--- Soulful/App_Start/BundleConfig.cs
+++ Soulful/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace Soulful
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/Assest/js").Include(
+                       "~/Assest/js/navbar.js"));
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
 
