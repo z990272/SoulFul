fa80ecc63364ba7b5ee6aa81dbc4d0f76f0b0675
DianeLin <ncy924220@gmail.com>
2020-05-26 16:54:05 +0800

    /

---
+++ Soulful/Models/Song.cs
@@ -0,0 +1,27 @@
+namespace Soulful
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Song")]
+    public partial class Song
+    {
+        [Key]
+        public int Song_id { get; set; }
+
+        public int Album_id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Song_name { get; set; }
+
+        public int Language_id { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public virtual Language Language { get; set; }
+    }
+}
