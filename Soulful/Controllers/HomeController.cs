dfce7b5e04e862ec3b2ef3790356f240b2ae30c2
2opex <jerry1003tw@gmail.com>
2020-05-31 18:03:21 +0800

    不會的可以看一下,HomeController,Home/Index,HomeCardPartialView參考看看

--- Soulful/Controllers/HomeController.cs
+++ Soulful/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Soulful.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,18 @@ namespace Soulful.Controllers
 {
     public class HomeController : Controller
     {
+        private SoulfulContext context = new SoulfulContext();
         public ActionResult Index()
         {
-            return View();
+
+            var AlbumDetail = from album in context.Album.ToList()
+                              join singer in context.Singer.ToList()
+                              on album.Singer_id equals singer.Singer_id
+                              select album;
+            
+            return View(AlbumDetail);
         }
 
-     
+
     }
 }
