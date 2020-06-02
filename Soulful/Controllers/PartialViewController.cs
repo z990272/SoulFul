be1389296901c45154dcc3d54a84fa8d7f4fae4d
z990272 <z990272@gmail.com>
2020-06-02 11:55:31 +0800

    EventCard

---
+++ Soulful/Controllers/PartialViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Soulful.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EventCard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Soulful/ViewModels/EventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Soulful.ViewModels
+{
+    public class EventViewModel
+    {
+        public string Name { get; set; }
+
+        public 
+    }
+}
