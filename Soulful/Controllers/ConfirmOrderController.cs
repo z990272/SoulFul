f9a6ce4f4988851a4f8be46d982a82329d9fda09
DianeLin <ncy924220@gmail.com>
2020-06-02 16:04:15 +0800

    新增訂單確認與結帳頁面

---
+++ Soulful/Controllers/ConfirmOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Soulful.Controllers
+{
+    public class ConfirmOrderController : Controller
+    {
+        // GET: ConfirmOrder
+        public ActionResult ConfirmOrderView()
+        {
+            return View();
+        }
+    }
+}
