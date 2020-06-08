c5609cb0914612d1e6308360f231c74986d94634
z990272 <z990272@gmail.com>
2020-06-08 14:30:04 +0800

    document

--- Soulful/Asset/js/Layout.js
+++ Soulful/Asset/js/Layout.js
@@ -1,3 +1,13 @@
 ﻿function Send() {
 
+    let nameElement = document.querySelector('#name');
+    let emailElement = document.querySelector('#email');
+    let subjectElement = document.querySelector('#subject');
+    let messageElement = document.querySelector('#message');
+
+    let name = nameElement.value;
+    let email = emailElement.value;
+    let subject = subjectElement.value;
+    let message = messageElement.value;
+  
 }
